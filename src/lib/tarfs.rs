ee0dcb9706ff019698b30efe0954bba8524158ec
Gero Posmyk-Leinemann <gero.posmyk-leinemann@typefox.io>
2019-08-08 19:30:29 +0000

    fix offset, ttl

--- src/lib/tarfs.rs
+++ src/lib/tarfs.rs
@@ -11,7 +11,7 @@ use time::Timespec;
 use libc::{ENOENT};
 
 use fuse;
-use fuse::{FileType, Filesystem, Request, ReplyAttr, ReplyEntry, ReplyDirectory};
+use fuse::{FileAttr, FileType, Filesystem, Request, ReplyAttr, ReplyEntry, ReplyDirectory};
 
 use tar::EntryType;
 
@@ -49,12 +49,16 @@ impl<'f> Filesystem for TarFs<'f> {
         let node = match self.index.lookup_child(parent, PathBuf::from(name)) {
             Some(a) => a,
             None => {
-                reply.error(ENOENT);
-                debug!("lookup: no parent entry");
+                // According to https://github.com/libfuse/libfuse/blob/master/include/fuse_lowlevel.h#L60
+                // this enables caching of none-entries (negative caching)
+                let attrs = emtpy_attr();
+                reply.entry(&ttl_max(), &attrs, 0);
+                // reply.error(ENOENT);
+                debug!("lookup: no entry");
                 return;
             },
         };
-        reply.entry(&ttl(10), &node.attrs(), 0);
+        reply.entry(&ttl_max(), &node.attrs(), 0);
     }
 
     fn getattr(&mut self, _req: &Request, ino: u64, reply: ReplyAttr) {
@@ -69,7 +73,7 @@ impl<'f> Filesystem for TarFs<'f> {
             Some(n) => n,
         };
 
-        reply.attr(&ttl(10), &node.attrs());
+        reply.attr(&ttl_max(), &node.attrs());
     }
 
     fn readdir(&mut self, _req: &Request, ino: u64, fh: u64, offset: i64, mut reply: ReplyDirectory) {
@@ -89,16 +93,9 @@ impl<'f> Filesystem for TarFs<'f> {
             return
         }
 
-        let mut out_off: i64 = 1;
-        let get = |off: &mut i64| -> i64 {
-            let res = *off;
-            *off += 1;
-            res
-        };
-
         let mut full;
         if offset == 0 {
-            let off = get(&mut out_off);
+            let off = 1;
             let kind = FileType::Directory;
             full = reply.add(node.ino(), off, kind, ".");
             trace!("reply.add inode {}, offset {}, file_type {:?}, base {} ", ino, off, kind, ".");
@@ -115,7 +112,7 @@ impl<'f> Filesystem for TarFs<'f> {
                 Some(ino) => ino,
             };
 
-            let off = get(&mut out_off);
+            let off = 2;
             let kind = FileType::Directory;
             full = reply.add(ino, off, kind, "..");
             trace!("reply.add inode {}, offset {}, file_type {:?}, base {} ", ino, off, kind, "..");
@@ -125,14 +122,15 @@ impl<'f> Filesystem for TarFs<'f> {
             }
         }
 
-        let c_offset = (offset - 2).max(0) as usize;
-        for child in &node.children.borrow()[c_offset..] {
+        let children_offset = (offset - 2).max(0);
+        let mut off: i64 = 2 + children_offset + 1;
+        for child in &node.children.borrow()[children_offset as usize..] {
             let ino = child.ino();
             let kind = child.attrs().kind;
             let name = &child.entry.name;
-            let off = get(&mut out_off);
             trace!("reply.add inode {}, offset {}, file_type {:?}, base {} ", ino, off, kind, name.display());
             full = reply.add(ino, off, kind, name);
+            off += 1;
             if full {
                 break;
             }
@@ -164,6 +162,27 @@ impl<'f> Filesystem for TarFs<'f> {
     // }
 }
 
-fn ttl(secs: i64) -> Timespec {
-    Timespec::new(secs, 0)
+fn emtpy_attr() -> FileAttr {
+    FileAttr {
+        ino: 0,
+        size: 0,
+        blocks: 0,
+        atime: Timespec::new(0, 0),
+        mtime: Timespec::new(0, 0),
+        ctime: Timespec::new(0, 0),
+        crtime: Timespec::new(0, 0),
+        kind: FileType::RegularFile,
+        perm: 0,
+        nlink: 0,
+        uid: 0,
+        gid: 0,
+        rdev: 0,
+        flags: 0,
+    }
+}
+
+/// As tarfs is a static file system in which files will never change, we use the highest possible timeout for entries and attributes read by the kernel
+/// Reference: Here's the best documentation about timeouts I could find: https://github.com/libfuse/libfuse/blob/master/include/fuse_lowlevel.h#L90
+fn ttl_max() -> Timespec {
+    Timespec::new(std::i64::MAX, 0)
 }
