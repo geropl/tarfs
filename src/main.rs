8e6bb6b9f752825daf794de7f6118db058239b45
Gero Posmyk-Leinemann <gero.posmyk-leinemann@typefox.io>
2019-08-21 22:37:00 +0000

    Use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 extern crate env_logger;
+extern crate tarfslib as lib;
+extern crate clap;
 
-extern crate tarfslib;
-use tarfslib as lib;
+use clap::{App, Arg};
 
-use std::env;
-use std::path::Path;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<std::error::Error>>  {
+    let matches = App::new("tarfs")
+        .version("1.0")
+        .author("Gero Posmyk-Leinemann <geroleinemann@gmx.de>")
+        .about("A readonly FUSE filesystem that allows to mount tar files")
+        .arg(Arg::with_name("archive")
+            .short("a")
+            .long("archive")
+            .help("The tar file that should be mounted")
+            .required(true)
+            .takes_value(true)
+            .index(1))
+        .arg(Arg::with_name("mountpoint")
+            .short("m")
+            .long("mountpoint")
+            .help("The path to the directory where the archive should be mounted")
+            .required(true)
+            .takes_value(true)
+            .index(2))
+        .get_matches();
+
+    let filename = PathBuf::from(matches.value_of("archive").unwrap());
+    let mountpoint = PathBuf::from(matches.value_of("mountpoint").unwrap());
+
     env_logger::init();
-
-    let args: Vec<String> = env::args().collect();
-    let (filename, mountpoint) = match args.as_slice() {
-        [_, ref filename, ref mountpoint] => Ok((Path::new(filename), Path::new(mountpoint))),
-        _ => Err(format!("Usage: {} <FILENAME> <MOUNTPOINT>", args.as_slice()[0]))
-    }?;
-
-    lib::setup_tar_mount(filename, mountpoint, None)?;
+    lib::setup_tar_mount(&filename, &mountpoint, None)?;
 
     Ok(())
 }
